28555c49aa6721b82ee8fcab78810c633f38588f
simono41 <simono41>
2017-11-06 21:38:10 +0100

    final-fix

--- start-text.sh
+++ start-text.sh
@@ -4,14 +4,14 @@ set -ex
 
 while true
 do
-./cnee-play.sh text-check-TG
+./cnee-play.sh text-check-WA
 check=$(xclip -o)
 if [ "${check}" != "${message}" ]; then
 ./cnee-play.sh text-WA_TG
 message=$(xclip -o)
 fi
 sleep 10
-./cnee-play.sh text-check-WA
+./cnee-play.sh text-check-TG
 check=$(xclip -o)
 if [ "${check}" != "${message}" ]; then
 ./cnee-play.sh text-TG_WA
